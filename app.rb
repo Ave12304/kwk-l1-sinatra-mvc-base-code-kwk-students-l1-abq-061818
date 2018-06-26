0ed49079bffa4d94aa6255849a001bd9a4223710
Ave12304 <averyselover@hotmail.com>
2018-06-26 17:14:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 class IdeaBoxApp < Sinatra::Base
   get '/' do
-    "<h2>Hello, World!><blockquote>I quess I always felt even if the world came to an end McDonalds would still be open.<cite>Susan ///beth pefrrer</cite></blockquote>"
+    erb :index 
   end
 end
